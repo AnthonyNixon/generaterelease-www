ec6ab6eb3401ca624a912624d37679810fef295e
Anthony Nixon <ajn0592@gmail.com>
2017-03-19 21:40:51 -0500

    handle multiple releases

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ angular.module('releaseGeneratorApp', ['ngMaterial', 'ngRoute'])
               $scope.currentlyUpdating = true;
               $http.get("https://us-central1-releasegenerator.cloudfunctions.net/generaterelease?letter=" + letter)
                 .then(function(response){
-                  $scope.releaseName = response.data;
+                  $scope.releases = response.data;
                   $scope.currentlyUpdating = false;
                 });
             }
